c0f14b7172a514cf8552d07b4280a6eea9d966b1
PlaceRem <placerem74@gmail.com>
2019-01-14 12:46:26 +0500

    Initial Commit

---
+++ List Of News 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  List Of News 2.0
+//
+//  Created by Denis Kovrigin on 14/01/2019.
+//  Copyright © 2019 Denis Kovrigin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
